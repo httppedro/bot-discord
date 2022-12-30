69527419a489e311b69f94861983b0184382b9c5
Ransom <ransom@pop-os.localdomain>
2022-12-30 20:42:59 -0300

    v2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({intents: [1, 512, 32768, 2, 128]});
-const config = require("./config.json");
+const config = require("././config.json");
 const fs = require("fs");
 
 client.login(config.token)
